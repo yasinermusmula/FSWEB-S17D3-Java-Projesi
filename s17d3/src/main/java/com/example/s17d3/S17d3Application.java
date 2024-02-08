ac86cbc42162dfe02c44d37600496e2d89c88a61
Yasin Ermusmula <yasin.ermusmula1@gmail.com>
2024-02-08 17:15:33 +0300

    task done

---
+++ s17d3/src/main/java/com/example/s17d3/S17d3Application.java
@@ -0,0 +1,13 @@
+package com.example.s17d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S17d3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S17d3Application.class, args);
+	}
+
+}
